2f0d0108210672cfd81b32c2f303ff0ef6103d43
gooddayx <amazingspace@ya.ru>
2024-03-21 03:54:52 +0500

    Created UI intrface v_0_1 no background no script. Append new item

--- Assets/Scripts/LineForce.cs
+++ Assets/Scripts/LineForce.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))] // Добавил после созвона.
+
 public class LineForce : MonoBehaviour {
     [SerializeField] private float shotPower;
     [SerializeField] private float stopVelocity = .05f; 
